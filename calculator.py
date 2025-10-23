33e269463e272cbab33c3b4d6fd316a2c2398ed0
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-24 00:31:10 +0530

    Add

--- calculator.py
+++ calculator.py
@@ -7,15 +7,6 @@ print(f"Subtraction: {a - b}")
 
 
 
-
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-
-print(f"Addition: {a + b}")
-print(f"Subtraction: {a - b}")
-print(f"Multiplication: {a * b}")
 if b != 0:
     print(f"Division: {a / b}")
 else:
