1d0c03e16540b3b21a161eaff4b3c7b38176b384
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-23 20:57:54 +0530

    add

--- calculator.py
+++ calculator.py
@@ -4,3 +4,19 @@ b = int(input("Enter second number: "))
 
 print(f"Addition: {a + b}")
 print(f"Subtraction: {a - b}")
+
+
+
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+if b != 0:
+    print(f"Division: {a / b}")
+else:
+    print("Division: Cannot divide by zero.")
